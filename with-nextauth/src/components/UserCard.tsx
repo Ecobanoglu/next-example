81b76e730008b6e15cfad79b48a97df24ff6c40e
Ergün Ç <ertbihal@gmail.com>
2023-10-11 17:45:09 +0300

    Update UserCard.tsx

--- with-nextauth/src/components/UserCard.tsx
+++ with-nextauth/src/components/UserCard.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
 
 type User =
   | {
       name?: string | null | undefined;
       email?: string | null | undefined;
-      image?: string | null | undefined;
     }
   | undefined;
 
